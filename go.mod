2c90e6ee167cb9ead4c1a339f1ffdb914322ceb1
johnstonjacob <johnstonajacob@gmail.com>
2018-08-30 02:10:12 +0000

    runner_server init

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/gopherpun/runner_server
+
+require (
+	github.com/Microsoft/go-winio v0.4.11 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/docker/distribution v2.6.2+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.3.3 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stevvooe/resumable v0.0.0-20170302213456-2aaf90b2ceea // indirect
+	github.com/streadway/amqp v0.0.0-20180806233856-70e15c650864
+	github.com/stretchr/testify v1.2.2 // indirect
+	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d // indirect
+	golang.org/x/sys v0.0.0-20180828065106-d99a578cf41b // indirect
+)
---
+++ runner.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+
+	"github.com/docker/docker/client"
+)
+
+func New(code string) string {
+	id := "go-runner"
+	err := writeCodeToFile(code)
+
+	if err != nil {
+		panic(err)
+	}
+
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	buildImage(cli, id)
+	x := buildContainer(cli, id)
+
+	y := getLogs(x, cli)
+	fmt.Println(y)
+
+	cleanup(cli, id)
+
+	return y
+
+}
+
+func buildImage(cli *client.Client, id string) {
+
+	config := types.ImageBuildOptions{Tags: []string{id}}
+	createBuildContext()
+	buildContext, err := os.Open("./tmp/tar/go.tar")
+	if err != nil {
+		panic(err)
+	}
+	defer buildContext.Close()
+
+	br, err := cli.ImageBuild(context.Background(), buildContext, config)
+	if err != nil {
+		panic(err)
+	}
+	defer br.Body.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(br.Body)
+	s := buf.String()
+	fmt.Println(s) //do not remove
+
+}
+
+func createBuildContext() {
+	file, err := os.Create("./tmp/tar/go.tar")
+	if err != nil {
+		panic(err)
+	}
+
+	gzipWriter := gzip.NewWriter(file)
+	defer gzipWriter.Close()
+
+	tw := tar.NewWriter(gzipWriter)
+	defer tw.Close()
+
+	files := map[string][]byte{"Dockerfile": nil, "main.go": nil}
+	for k, v := range files {
+		a, err := ioutil.ReadFile("./tmp/go/" + k)
+		v = a
+		if err != nil {
+			panic(err)
+		}
+		hdr := &tar.Header{Name: k, Mode: 0600, Size: int64(len(v))}
+
+		if err := tw.WriteHeader(hdr); err != nil {
+			panic(err)
+		}
+
+		if _, err := tw.Write([]byte(v)); err != nil {
+			panic(err)
+		}
+
+	}
+
+}
+
+func cleanup(cli *client.Client, id string) error {
+	config := types.ContainerRemoveOptions{Force: true}
+	ctx := context.Background()
+
+	err := cli.ContainerRemove(ctx, id, config)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = cli.ImageRemove(ctx, id, types.ImageRemoveOptions{Force: true})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeCodeToFile(code string) error {
+	path := "./tmp/go/main.go"
+	os.Remove(path)
+	os.Create(path)
+	f, err := os.OpenFile(path, os.O_RDWR, 0777)
+
+	if err != nil {
+		return err
+	}
+
+	if _, err := f.WriteString(code); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func buildContainer(cli *client.Client, id string) string {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	config := container.Config{Image: id}
+	hostconfig := container.HostConfig{}
+	netconfig := network.NetworkingConfig{}
+
+	c, err := cli.ContainerCreate(ctx, &config, &hostconfig, &netconfig, id)
+	if err != nil {
+		panic(err)
+	}
+	if err := cli.ContainerStart(ctx, c.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	return c.ID
+}
+
+func getLogs(id string, cli *client.Client) string {
+	var b strings.Builder
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	reader, err := cli.ContainerLogs(ctx, id, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = io.Copy(&b, reader)
+	if err != nil && err != io.EOF {
+		fmt.Println(err)
+	}
+
+	return b.String()
+}
